980bce2768747e3944f6fc3c55d375825fdd5fa2
LeXXeR <lex1977@gmail.com>
2022-09-15 21:17:33 +0700

    second commit

--- src/main/java/FileProcessing/FileIO.java
+++ src/main/java/FileProcessing/FileIO.java
@@ -1,9 +1,23 @@
 package FileProcessing;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileIO {
 
     public static void main(String[] args) {
-        System.out.println(123);
+
+        //Path path = Paths.get("src/main/resourses/testIO.txt");
+        Path path = Paths.get("d:\\\\temp\\testIO.txt");
+        try(BufferedWriter writer = Files.newBufferedWriter(path, Charset.forName("UTF-8"))){
+            writer.write("To be, or not to be. That is the question.");
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
     }
 
 }
